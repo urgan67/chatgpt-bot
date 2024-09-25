1fd38a7da7662e51b532cd32e467459c21c4bac2
Urgan67 <evgenyurganov67@gmail.com>
2024-09-25 16:03:56 +0300

    1312312

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from worker import add_data_user, get_data_user,update_data_user
+from worker import add_data_user, get_data_user,update_data_user,add_session
 
 
 
@@ -12,17 +12,27 @@ from worker import add_data_user, get_data_user,update_data_user
 data = {
     "user_id": 212580111,
     "name": 'Ann',
-    "cash": 100,
+    "cash": 150,
+    "session_id": 1,
+    "tokens": 10,
+    "price": 2,
+    
 }
 user_id = data.get('user_id')
 name = data.get('name')
 cash = data.get('cash')
+session_id = data.get('session_id')
+tokens = data.get('tokens')
+price = data.get('price')
+
 print(user_id, name, cash)
 
 # Запуск функции:
 # confirm = add_data_user(data)
+
 # confirm = get_data_user(21258098)
-confirm = update_data_user(212580)
+# confirm = update_data_user(212580)
+confirm = add_session(data)
 # if confirm is True:
 #     print("Пользователь {user_id} {name} внесен в таблицу users.")
 # else:
--- app/worker.py
+++ app/worker.py
@@ -2,16 +2,7 @@
 import sqlite3
 conn = sqlite3.connect('./db/db.litesql')
 
-data = {
-    "user_id": 212580,
-    "name": 'Ann',
-    "cash": 10,
-}
-# user_id = data.get('user_id')
-# name = data.get('name')
-# cash = data.get('cash')
-
-#### USERS: ####
+#### USERS:
 
 # Чтение USERS:
 def get_data_user(data):
@@ -56,6 +47,7 @@ def update_data_user(data):
             ''',
             (data.get('user_id'), data.get('name'), data.get('cash'))
         )
+        conn.commit()
         cursor.close()
         return f"{cursor.rowcount} record(s) updated."
     except Exception as e:
@@ -63,32 +55,39 @@ def update_data_user(data):
 
 
 
-#### SESIONS: ####
+#### SESSIONS:
 
-# Read sesion:
+# Add session:
 
+def add_session(conn, data, user_id):
+    cursor = conn.cursor()
+    try:
+        cursor.execute('SELECT * FROM users WHERE user_id = ?;', (user_id,))
+        user = cursor.fetchone()
+        if user is None:
+            print(f"Пользователь с user_id {data} не найден.")
+            return False
 
+        cursor.execute('''
+            INSERT INTO session (user_id, model, tokens, price)
+            VALUES (?, ?, ?, ?);
+        ''', (data.get('user_id'))
+        )
+        conn.commit()
+        print(f"Сессия для пользователя с user_id {data} успешно добавлена.")
+        return True
+    except sqlite3.Error as e:
+        print(f"Ошибка при добавлении сессии для пользователя с user_id {data}: {e}")
+        return False
+    
+# Read sessions:
 
+# def get_sessions(conn, user_id):
+#     cursor = conn.cursor()
+#     cursor.execute('SELECT * FROM session WHERE user_id = ?;', (user_id,))
+#     sessions = cursor.fetchall()
+#     cursor.close()
 
-    # if user:
-    #     print(f"Пользователь с user_id {user_id} найден: {user}")
-    # else:
-    #     print(f"Пользователь с user_id {user_id} не найден.")
-# #                     ### Функция для получения пользователя по user_id ###
-# # def get_user(conn, user_id):
-# #     cursor = conn.cursor()
-# #     try:
-# #         cursor.execute('SELECT * FROM users WHERE user_id = ?;', (user_id,))
-# #         user = cursor.fetchone()
-# #         if user:
-# #             print(f"Пользователь с user_id {user_id} найден: {user}")
-# #         else:
-# #             print(f"Пользователь с user_id {user_id} не найден.")
-# #         return user
-# #     except sqlite3.Error as e:
-# #         print(f"Ошибка при получении пользователя с user_id {user_id}: {e}")
-# #         return None
-# # cursor.close()
 
 
 # # Функция для добавления сессии
@@ -158,23 +157,3 @@ def update_data_user(data):
 # #     except sqlite3.Error as e:
 # #         print(f"Ошибка при получении сессий для пользователя с user_id {user_id}: {e}")
 # #         return []
-# def base():
-#     mock_conn = MagicMock()
-
-#     # Добавление пользователей
-#     add_user(mock_conn, 'Nick', 50.0)
-#     # add_user(mock_conn, 'Alice', 100.0)
-
-#     # Получение и работа с пользователями
-#     get_user(mock_conn, 1)
-#     # add_session(mock_conn, 1, 'model1')
-#     # get_sessions(mock_conn, 1)
-
-#     # get_user(mock_conn, 2)
-#     add_session(mock_conn, 2, 'model2')
-#     get_sessions(mock_conn, 2)
-
-# if __name__ == '__main__':
-#     base()
-# # Закрываем соединение с базой данных
-# conn.close()
