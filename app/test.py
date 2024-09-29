a1d4414217605de2be77aa50c7aebeccd8d82409
Urgan67 <evgenyurganov67@gmail.com>
2024-09-29 16:03:40 +0300

    1

--- app/test.py
+++ app/test.py
@@ -2,20 +2,20 @@ from worker import get_data_user,update_data_user,add_data_user
 
 
 data = {
-    "user_id": 14,
-    "name": 'хooorr',
-    "cash": 10,  
+    # "user_id": 17,
+    "name": 'х',
+    "cash": 100,  
 }
 
 # print(data.get("name"))
 
-# confirm = add_data_user(data)
-# print(data)
+confirm = add_data_user(data)
+print(data)
 
-user_id = 14  # Замените на нужный user_id
-new_cash_value = 100.0  # Замените на нужное значение cash
-result = update_data_user(data)
+# user_id = 14  # Замените на нужный user_id
+# new_cash_value = 100.0  # Замените на нужное значение cash
+# result = update_data_user(data)
 
-user_id = 14
-confirm = get_data_user(user_id)
-print(confirm)
+# user_id = 14
+# confirm = get_data_user(user_id)
+# print(confirm)
--- app/worker.py
+++ app/worker.py
@@ -23,37 +23,45 @@ def get_data_user(user_id):
 # Добавление USERS:
 
 def add_data_user(data):
-
-    # Инициализируем списки для ключей и значений
-    key_dict, value_dict = [],[]
-
-    # Формирование списков ключей и значений
-    for key, value in data.items():
-        if key != "user_id": 
-            key_dict.append(key)
-            value_dict.append(value)
-
-    # Преобразуем список ключей в строку, разделенную запятыми
-    key_dict = ", ".join(key_dict)
-
-    # Подготавливаем строки для плейсхолдеров значений
-    value_dict = ", ".join(['?'] * len(value_dict))
-# 
+    
     try:
+
+        user_id = data.get("user_id")
+
+        if not user_id:
+            return False 
+    
+        keys = []
+        values = []
+        drop = []
+
+        for key, value in data.items():
+            if key != "user_id": 
+                keys.append(key)
+            values.append(value)
+            drop.append("?")
+            
+    # Преобразуем списки ключей и плейсхолдеров в строки, разделенные запятыми
+        key_str = ", ".join(keys)
+        drops = ", ".join(drop)
+  
         cursor = conn.cursor()
         cursor.execute(
             f'''
-            INSERT INTO users ({key_dict}) 
-            VALUES ({value_dict})
-            ''', value_dict
+            INSERT INTO users ({key_str}) VALUES ({drops})
+            ''', 
+            values
         )
-
         conn.commit()
         cursor.close()
+        print(key_str)
+        print(values)
         return True
     except Exception as e:
         print(f"Ошибка: {e}")
         return False
+
+
 
 
 # Обновление данных USERS:
@@ -69,15 +77,15 @@ def update_data_user(data):
     for key, value in data.items():
         if key != "user_id":
             key_dict.append(f"{key} = ?")
-            value_dic.append(value)
+        value_dic.append(value)
 
     
     key_dict = ", ".join(key_dict)
   
-    value_dict = ", ".join(['?'] * len(value_dict))
-    print(key_dict)
-    # print()
-    print(value_dic)
+    # value_dict = ", ".join(['?'] * len(value_dict))
+    # print(key_dict)
+    # # print()
+    # print(value_dic)
 
     try:
         cursor = conn.cursor()
