383fa7aff49f83dd5c0cded73541b5bc207bf31d
urgan67 <evgenyurganov67@gmail.com>
2024-10-26 17:20:25 +0300

    1

--- app/bot.py
+++ app/bot.py
@@ -20,6 +20,7 @@ async def typing(action) -> None:
     await bot.send_chat_action(action.chat.id, action='typing')
 
 
+
 # Команда /start
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -85,41 +86,41 @@ async def ask_gpt(message: types.Message):
     text = message.text
 
     price = {                              # при курсе 1$ = 100 руб.
-    'gpt-4-turbo': 0.08,               # 7.22 руб.
-    'gpt-4-turbo-2024-04-09': 0.08,    # 7.22 руб.
-    'tts-1': 0.01,                     # 1 руб.
-    'tts-1-1106': 0.01,                # 1 руб.
-    'chatgpt-4o-latest': 0.04,         # 4 руб.
-    'dall-e-2': 0.08,                  # 7.22 руб.
-    'whisper-1': 0.006,                # 0.6 руб.
-    'gpt-4-turbo-preview': 0.08,       # 7.22 руб.
-    'gpt-3.5-turbo-instruct': 0.005,   # 0.5 руб.
-    'gpt-4-0125-preview': 0.08,        # 7.22 руб.
-    'gpt-3.5-turbo-0125': 0.004,       # 0.4 руб.
-    'gpt-4o-2024-08-06': 0.04,         # 4 руб.
-    'gpt-3.5-turbo': 0.0015,           # 0.15 руб.
-    'gpt-4o': 0.04,                    # 4 руб.
-    'babbage-002': 0.002,              # 0.2 руб.
-    'davinci-002': 0.03,               # 3 руб.
-    'gpt-4o-realtime-preview-2024-10-01': 0.05,  # 5 руб.
-    'dall-e-3': 0.09,                  # 8.13 руб.
-    'gpt-4o-realtime-preview': 0.05,   # 5 руб.
-    'gpt-4o-mini': 0.0015,             # 0.15 руб.
-    'gpt-4o-2024-05-13': 0.04,         # 4 руб.
-    'gpt-4o-mini-2024-07-18': 0.0015,  # 0.15 руб.
-    'gpt-4o-audio-preview-2024-10-01': 0.02,  # 2 руб.
-    'gpt-4o-audio-preview': 0.02,      # 2 руб.
-    'tts-1-hd': 0.015,                 # 1.5 руб.
-    'tts-1-hd-1106': 0.015,            # 1.5 руб.
-    'gpt-4-1106-preview': 0.08,        # 7.22 руб.
-    'text-embedding-ada-002': 0.0008,  # 0.08 руб.
-    'gpt-3.5-turbo-16k': 0.004,        # 0.4 руб.
-    'text-embedding-3-small': 0.0005,  # 0.05 руб.
-    'text-embedding-3-large': 0.003,   # 0.3 руб.
-    'gpt-3.5-turbo-1106': 0.004,       # 0.4 руб.
-    'gpt-4-0613': 0.18,                # 16.24 руб.
-    'gpt-4': 0.18,                     # 16.24 руб.
-    'gpt-3.5-turbo-instruct-0914': 0.005,  # 0.5 руб.
+    'gpt-4-turbo': 7.22,               # 7.22 руб.
+    'gpt-4-turbo-2024-04-09': 7.22,    # 7.22 руб.
+    'tts-1': 1,                     # 1 руб.
+    'tts-1-1106': 1,                # 1 руб.
+    'chatgpt-4o-latest': 4,         # 4 руб.
+    'dall-e-2': 7.22,                  # 7.22 руб.
+    'whisper-1': 0.6,                # 0.6 руб.
+    'gpt-4-turbo-preview': 7.22,       # 7.22 руб.
+    'gpt-3.5-turbo-instruct': 0.5,   # 0.5 руб.
+    'gpt-4-0125-preview': 7.22,        # 7.22 руб.
+    'gpt-3.5-turbo-0125': 0.4,       # 0.4 руб.
+    'gpt-4o-2024-08-06': 4,         # 4 руб.
+    'gpt-3.5-turbo': 0.15,           # 0.15 руб.
+    'gpt-4o': 4,                    # 4 руб.
+    'babbage-002': 0.2,              # 0.2 руб.
+    'davinci-002': 3,               # 3 руб.
+    'gpt-4o-realtime-preview-2024-10-01': 5,  # 5 руб.
+    'dall-e-3': 8.13,                  # 8.13 руб.
+    'gpt-4o-realtime-preview': 5,   # 5 руб.
+    'gpt-4o-mini': 0.15,             # 0.15 руб.
+    'gpt-4o-2024-05-13': 4,         # 4 руб.
+    'gpt-4o-mini-2024-07-18': 0.15,  # 0.15 руб.
+    'gpt-4o-audio-preview-2024-10-01': 2,  # 2 руб.
+    'gpt-4o-audio-preview': 2,      # 2 руб.
+    'tts-1-hd': 1.5,                 # 1.5 руб.
+    'tts-1-hd-1106': 1.5,            # 1.5 руб.
+    'gpt-4-1106-preview': 7.22,        # 7.22 руб.
+    'text-embedding-ada-002': 0.08,  # 0.08 руб.
+    'gpt-3.5-turbo-16k': 0.4,        # 0.4 руб.
+    'text-embedding-3-small': 0.05,  # 0.05 руб.
+    'text-embedding-3-large': 0.3,   # 0.3 руб.
+    'gpt-3.5-turbo-1106': 0.4,       # 0.4 руб.
+    'gpt-4-0613': 16.24,                # 16.24 руб.
+    'gpt-4': 16.24,                     # 16.24 руб.
+    'gpt-3.5-turbo-instruct-0914': 0.5,  # 0.5 руб.
 }
 
 
@@ -137,7 +138,7 @@ async def ask_gpt(message: types.Message):
     if str(id) in white_list:
         flag = True
         
-    money = data["money"]
+    money = data.get("money")
 
     if money <=0 and flag == False:
         await bot.send_message(message.chat.id, "Извините, на счете не достаточно средств")
@@ -149,18 +150,18 @@ async def ask_gpt(message: types.Message):
             await message.answer(response.get("gpt_response", 'total_tokens'), markdown = 'markdown')
             if flag == True:
                 return
-            total_tokens = response["total_tokens"]
-            model_used = response["model"]
+            total_tokens = response.get("total_tokens")
+            model_used = response.get("model")
             tok_1_rub = price.get(model_used, 0) / 1000
-            all_token = total_tokens * tok_1_rub
+            total_coast = total_tokens * tok_1_rub
 
-            new_money = money - all_token
+            new_money = money - total_coast
             new_data = {"user_id": id, "money": new_money}
 
             await update_user(new_data)
             await bot.send_message(
                     message.chat.id, 
-                    f"Потрачено {total_tokens} токенов. С вашего счета списано {all_token:.2f} руб. Остаток: {new_money:.2f} руб."
+                    f"Потрачено {total_tokens} токенов. С вашего счета списано {total_coast:.4f} руб. Остаток: {new_money:.4f} руб."
                 )
 
             return
